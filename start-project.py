00f7a8c5e604ff02cf742d609508f7c476aa793c
JustinTadlock <justin.b.tadlock@gmail.com>
2019-08-03 23:41:30 -0700

    Update to use google_authentication.py

--- start-project.py
+++ start-project.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template, url_for, redirect, jsonify, flash, request
-from flask import session as login_session
+from flask import (
+    Flask, 
+    session as login_session,
+    render_template, 
+    url_for, 
+    redirect, 
+    jsonify, 
+    flash, 
+    request,
+    make_response
+)
+
 from database_setup import Base, Restaurant, MenuItem
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 import psycopg2
 import random, string
-
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.client import FlowExchangeError
 import httplib2
 import json
-from flask import make_response
 import requests
 
-CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+import google_authentication as gAuth
+
 
 app = Flask(__name__)
+try:
+    app.secret_key = open('secret_key.txt', 'r').read()
+except:
+    print('Error: Please create a \'secret_key.txt\' file within the app\'s directory')
 
 # Adding db functionality for CRUD operations
 engine = create_engine('sqlite:///restaurantmenu.db?check_same_thread=False')
@@ -26,102 +38,12 @@ session = DBsession()
 
 @app.route('/login')
 def Login():
-    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(1,32))
-    login_session['state'] = state
-    
-    return render_template('login.html', STATE=state)
+    return render_template('login.html', client_id=gAuth.CLIENT_ID)
 
-@app.route('/gdisconnect', methods=['POST'])
-def Google_Logout():
-    if login_session.get('access_token') is not None and login_session.get('connect_id') is not None:
-        for property in login_session.keys():
-            login_session.pop(login_session[property], None)
 
-        flash('You have been successfully logged out.')
-    
-    return redirect(url_for('Show_All_Restaurants'))
-
-@app.route('/gconnect', methods=['POST'])
+@app.route(gAuth.CLIENT_REDIRECT, methods=['POST'])
 def Google_Login():
-    if request.args.get('state') != login_session['state']:
-        response = make_response(json.dumps('Invalid state parameter'), 401)
-
-        response.headers['content-type'] = 'application/json'
-
-        return response
-    else:
-        code = request.data
-
-        # Update the authentication code
-        try:
-            oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
-            oauth_flow.redirect_uri = 'postmessage'
-
-            credentials = oauth_flow.step2_exchange(code)
-        except FlowExchangeError:
-            response = make_response(json.dumps('Failed to update the authentication code'), 401)
-            response.headers['content-type'] = 'application/json'
-            dir(response)
-            
-            return response
-
-        # Validate token code
-        access_token = credentials.access_token
-        url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
-        h = httplib2.Http()
-        result = json.loads(h.request(url, 'GET')[1])
-
-        if result.get('error') is not None:
-            response = make_response(json.dumps('Error: Could not validate the access token'), 401)
-            response.headers['content-type'] = 'application/json'
-            
-            return response
-
-        # Verify token for a given user
-        user_id = credentials.id_token['sub']
-        if result['user_id'] != user_id:
-            response = make_response(json.dumps('Error: The user ID does not match the given ID.'), 401)
-            response.headers['content-type'] = 'application/json'
-
-            return response
-        
-        # Verify the token is valid for this app
-        if result['issued_to'] != CLIENT_ID:
-            response = make_response(json.dumps('Error: Client ID does not match the app\'s id.'), 401)
-            response.headers['content-type'] = 'application/json'
-            print("Error: The client ID does not match the App's ID")
-
-            return response
-        stored_access_token = login_session.get('access_token')
-        stored_connect_id = login_session.get('connect_id')
-        if stored_access_token is not None and stored_connect_id is not None:
-            response = make_response(json.dumps('User is already connected'), 200)
-            response.headers['content-type'] = 'application/json'
-            response.headers['status'] = 200
-
-            return response
-
-        # Store the token and id for later use
-        login_session['access_token'] = credentials.access_token
-        login_session['connect_id'] = user_id
-
-        # Get user info
-        user_info_uri = "https://www.googleapis.com/oauth2/v1/userinfo"
-        params = { 'access_token': credentials.access_token, 'alt':'json' }
-        answer = requests.get(user_info_uri, params=params)
-        data = answer.json()
-
-        # Set the login session user info
-        login_session['username'] = data['name']
-        login_session['picture'] = data['picture']
-        login_session['email'] = data['email']
-
-        flash('You are successfully logged in!')
-
-        response = make_response(json.dumps('Logged in successfuly!'), status=200)
-        response.headers['content-type'] = 'application/json'
-
-        return response
+        return gAuth.Authentication_Callback()
 
 def Get_Restaurant_Data(rest_id):
     restaurant = session.query(Restaurant).filter_by(id=rest_id).one()
@@ -143,14 +65,12 @@ def Get_Restaurant_Data(rest_id):
 @app.route('/restaurants/', methods=['GET', 'POST'])
 @app.route('/', methods=['GET', 'POST'])
 def Show_All_Restaurants():
-
-    if request.method == 'POST':
-        print(login_session['state'])
-        login_session.pop('state', None)
-
     restaurants = session.query(Restaurant).all()
     
-    return render_template('show-all-restaurants.html', restaurants=restaurants)
+    return render_template(
+        'show-all-restaurants.html', 
+        restaurants=restaurants, 
+        authenticated=gAuth.Is_Authenticated())
 
 
 @app.route('/restaurant/<int:rest_id>/')
@@ -168,7 +88,7 @@ def Show_Restaurant(rest_id):
 
 @app.route('/restaurant/add/', methods=['GET', 'POST'])
 def Add_Restaurant():
-    if request.method == 'POST':
+    if request.method == 'POST' and gAuth.Is_Authenticated():
         new_restaurant = Restaurant(name = request.form['rest_name'])
 
         session.add(new_restaurant)
@@ -177,8 +97,10 @@ def Add_Restaurant():
         flash('Successfully added %s.' % (new_restaurant.name))
 
         return redirect(url_for('Show_All_Restaurants'))
-
-    return render_template('add-restaurant.html')
+    elif gAuth.Is_Authenticated():
+        return render_template('add-restaurant.html')
+    else:
+        return url_for('Show_All_Restaurants')
 
 
 @app.route('/restaurant/<int:rest_id>/edit', methods=['GET', 'POST'])
